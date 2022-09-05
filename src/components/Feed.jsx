f6e6e7fbca37592b173bc9a8d68c9563315086ff
akshay-Kalekar <akalekar2003@gmail.com>
2022-09-05 23:56:44 +0530

    Feed Done API left

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {useState,useEffect} from 'react'
 import{Box,Stack,Typography} from '@mui/material'
-import Sidebar from './Sidebar'
+import {Sidebar,Videos } from './'
 
 const Feed = () =>(
     <Stack sx={{flexDirection :{sx:"column", md:"row"}}}>
@@ -16,6 +16,13 @@ const Feed = () =>(
     </span>
     </Typography>
 
+    </Box>
+    <Box p={2} sx={{overflowY:'auto',height:'90vh', flex:2}}>
+    <Typography variant='h4'
+    fontWeight="bold" mb={2} sx={{color:'white'}}>
+    New <span style={{color:'#F31503'}}>Videos</span>
+    </Typography>
+    <Videos videos={[]}/>
     </Box>
     </Stack>
   )
