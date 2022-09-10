5c8daf80025848a5ba0f832a4e71090ee41033ef
akshay-Kalekar <akalekar2003@gmail.com>
2022-09-10 21:59:04 +0530

    modified:   src/components/ChannelCard.jsx 	modified:   src/components/ChannelDetail.jsx

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 import { demoProfilePicture } from "../utils/constants";
 
 // </Link>
-const ChannelCard = ({ channelDetail }) => (
+const ChannelCard = ({ channelDetail ,marginTop}) => (
   <Box
     sx={{
       boxShadow: "none",
@@ -16,9 +16,10 @@ const ChannelCard = ({ channelDetail }) => (
       width: { xs: "356px", md: "320px" },
       height: "326px",
       margin: "auto",
+      marginTop ,
     }}
   >
-    <Link to={`/channel/${channelDetail.id.channelId}`}>
+    <Link to={`/channel/${channelDetail?.id?.channelId}`}>
       <CardContent
         sx={{
           display: "flex",
