b03448e57d0ae7dfd2f8336a9415086dd5531a03
akshay-Kalekar <akalekar2003@gmail.com>
2022-09-11 12:49:55 +0530

    Added Side Video recommendation

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { Stack, Box } from "@mui/material";
 import { VideoCard, ChannelCard } from "./";
 
-const Videos = ({ videos }) => {
-  console.log(videos);
+const Videos = ({ videos,direction }) => {
+  //if(!videos?.lenght) return <div>Loading</div>;
 
   return (
-    <Stack direction="row" flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
+    <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
       {videos.map((item, idx) => (
         <Box key={idx}>
           {item.id.videoId && <VideoCard video={item} />}
@@ -14,7 +14,7 @@ const Videos = ({ videos }) => {
         </Box>
   ))}
     </Stack>
-   
+
   );
 }
 
